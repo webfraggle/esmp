5cc418aa7cc3f6556940514b9248b16ac4e6694f
Christoph Ketzler <christoph@ketzler.de>
2022-07-17 15:42:19 +0200

    Update savevalue.inc.php

--- PHP/savevalue.inc.php
+++ PHP/savevalue.inc.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['val']))
 {
-    file_put_contents('values.txt', date("Y-m-d H:i:s")."\t".$_GET['val'], FILE_APPEND);
+    file_put_contents('values.txt', date("Y-m-d H:i:s")."\t".$_GET['val']."\n", FILE_APPEND);
 }
 ?>
